df28d823582409dce8a272d6b3bae1d2eceb7385
IcyCrow <pavelkucan@gmail.com>
2026-03-14 17:58:34 +0700

    Shift Trieste middle arcs left

--- public/app.js
+++ public/app.js
@@ -473,7 +473,7 @@ const points = [
             target: 0,
             size: 74,
             thickness: 24,
-            colors: ['transparent', 'transparent', 'transparent', '#d7c8c6', '#d7c8c6', '#d7c8c6', 'transparent', '#d7c8c6', '#d7c8c6', 'transparent', 'transparent', '#d7c8c6']
+            colors: ['transparent', 'transparent', '#d7c8c6', '#d7c8c6', '#d7c8c6', 'transparent', '#d7c8c6', '#d7c8c6', 'transparent', 'transparent', '#d7c8c6', 'transparent']
           },
           {
             id: 'inner',
